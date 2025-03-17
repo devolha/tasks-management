73525dcc6e5405310f2539de4d2c3bf17a7c5369
devolha <sokolowskaoa@gmail.com>
2025-03-17 21:06:08 +0100

    initial commit

--- tasks.py
+++ tasks.py
@@ -10,19 +10,24 @@ def options_menu():
     return options
     
 def add_task(task_list):
-    number = int(input("Write a number of task: "))
-    priority = input("Choose priority: ")
-    name = input("Write the name of your task: ")
-    date = input("Write a date of task complition: ")
-
-    if number in task_list:
-        print(f"You already have task number {number}. Please choose another number.")
-    else: 
+    if not task_list:
+        print("You have no tasks yet. Don't worry, let's write the first one!")
+        number = int(input("Write a number of task: "))
+        priority = input("Choose priority: ")
+        name = input("Write the name of your task: ")
+        date = input("Write a date of task complition: ") 
         task_list[number] = {
             "date": date,
             "priority": priority,
             "name": name}
-        print("Great! Your task was written!")
+        print("\nGreat! Your task was written!")
+        print(task_list)
+    else: 
+        print("\nYou can choose another options. What's next?")
+
+    if number in task_list:
+        print(f"You already have task number {number}. Please choose another number.")
+    
 
 def delete_task():
     return None
@@ -50,18 +55,22 @@ user_input = input()
 while True:
     try:
         if user_input == "a":
-            print("You have no tasks yet. By don't worry, let's write the first one!" 
-                  if not task_list else task_list)
             add_task(task_list)
+            
         elif user_input == "b":
             delete_task()
+            print("You can choose another option or exit the program. What's next?")
+            
         elif user_input == "c":
             show_tasks()
+            print("You can choose another option or exit the program. What's next?")
+            
         elif user_input == "d":
             print("Thanks for your time. If you want to write a new task, feel free to use this script again!")
             break
     except (ValueError, SyntaxError, NameError):
         print ("Please choose options from the list")
+        break
      
     # Print Menu
     # Ask for user choice
